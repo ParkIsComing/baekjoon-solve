b981f04ffcd76f182dba84903cf0a65c3fb77c45
Gayeong Park <maroony55@gmail.com>
2024-01-16 16:53:54 +0900

    [level Lv.4] Title: 가사 검색, Time: 232.74 ms, Memory: 82.9 MB -BaekjoonHub

--- 프로그래머스/Lv.4/60060. 가사 검색/가사 검색.java
+++ 프로그래머스/Lv.4/60060. 가사 검색/가사 검색.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 class Solution {
-    public int lowerBound(ArrayList<String> list, String target, int start, int end) {
+    public int lowerBound(List<String> list, String target, int start, int end) {
         while (start < end) {
             int mid = (start + end) / 2;
             // arr[mid]가 target보다 사전순으로 같거나 뒤에 있는 경우
@@ -14,7 +14,7 @@ class Solution {
         return end;
     }
     
-    public int upperBound(ArrayList<String> list, String target, int start, int end) {
+    public int upperBound(List<String> list, String target, int start, int end) {
         while (start < end) {
             int mid = (start+end) / 2;
             // arr[mid]가 target보다 사전순으로 뒤에 있다면
@@ -27,59 +27,64 @@ class Solution {
         return end;
     }
     
-    public int countByRange(ArrayList<String> list, String leftValue, String rightValue) {
+    public int countByRange(List<String> list, String leftValue, String rightValue) {
         int rightIndex = upperBound(list, rightValue, 0, list.size());
         int leftIndex = lowerBound(list, leftValue, 0, list.size());
         return rightIndex - leftIndex;
     }
     
     // 모든 단어들을 길이 기준으로 나눠서 저장
-    public ArrayList<ArrayList<String>> list = new ArrayList<ArrayList<String>>();
+    public Map<Integer, List<String>> map = new HashMap<>();
     
     // 모든 단어들을 길이 기준으로 나눠서 뒤집어 저장 (접두사에 와일드카드가 붙은 경우를 위해)
-    public ArrayList<ArrayList<String>> reversedList = new ArrayList<ArrayList<String>>();
+    public Map<Integer, List<String>> reversedMap = new HashMap<>();
 
-    public int[] solution(String[] words, String[] queries) {
-        ArrayList<Integer> answers = new ArrayList<Integer>();
+    public List<Integer> solution(String[] words, String[] queries) {
+        List<Integer> answer = new ArrayList<>();
         
-        for (int i = 0; i<10001; i++) {
-            list.add(new ArrayList<String>());
-            reversedList.add(new ArrayList<String>());
+        // 있는 단어 길이 리스트만 생성
+        for (String word : words) {
+            int wordLen = word.length();
+            // 단어길이 키에 대한 값이 없으면 리스트 생성하고 해당 키에 맵핑
+            map.computeIfAbsent(wordLen, i -> new ArrayList<>()).add(word);
+            reversedMap.computeIfAbsent(wordLen, i -> new ArrayList<>()).add(reverseInput(word));
         }
         
-        for (int i=0; i < words.length; i++) {
-            String word = words[i];
-            list.get(word.length()).add(word); // 단어길이와 같은 인덱스값으로 삽입
-            
-            word = (new StringBuffer(word)).reverse().toString();
-            reversedList.get(word.length()).add(word);
-        }
-        
-        // 이진탐색을 위한 정렬 (같은 길이의 단어 리스트 내에서)
-        for (int i = 0; i < 10001; i++) {
-            Collections.sort(list.get(i));
-            Collections.sort(reversedList.get(i));
+        // 키별로 리스트 정렬
+        for (int key : map.keySet()) {
+            map.get(key).sort(null);
+            reversedMap.get(key).sort(null);
         }
         
         // 쿼리 하나씩 확인하며 처리
         for (int i = 0; i < queries.length; i++) {
+            List<String> list; // 맵의 리스트가 null인 경우를 처리하기 위해
             String q = queries[i];   
             int res = 0;
+            
             if (q.charAt(0) != '?') { // 접미사에 와일드 카드가 붙은 경우
-                res = countByRange(list.get(q.length()), q.replaceAll("\\?", "a"), q.replaceAll("\\?", "z")); // "fro??"라는 쿼리가 들어오면 "froaa" 보다 크거나 같으면서 "frozz" 보다 작거나 같은 단어의 개수를 센다.
+                list = map.get(q.length());
+                
             } else { // 접두사에 와일드카드가 붙은 경우
-                q = (new StringBuffer(q)).reverse().toString();
-                res = countByRange(reversedList.get(q.length()), q.replaceAll("\\?", "a"), q.replaceAll("\\?", "z"));                
+                q = reverseInput(q);
+                list = reversedMap.get(q.length());               
             }
             
-            answers.add(res);
+            if (list == null) {
+                answer.add(0);
+            } else {
+                res = countByRange(list, q.replaceAll("\\?", "a"), q.replaceAll("\\?", "z")); // "fro??"라는 쿼리가 들어오면 "froaa" 보다 크거나 같으면서 "frozz" 보다 작거나 같은 단어의 개수를 센다.
+            
+            answer.add(res);
+            }
+            
         }
         
-        // list -> array
-        int[] answer = new int[answers.size()];
-        for (int i = 0; i < answers.size(); i++) {
-            answer[i] = answers.get(i);
-        }
+        
         return answer;
+    }
+    
+    public String reverseInput(String s) {
+        return new StringBuilder(s).reverse().toString();
     }
 }
