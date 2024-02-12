2d1a7cefd767f897d9e17668658925e4d3288cda
Gayeong Park <maroony55@gmail.com>
2024-02-12 18:59:03 +0900

    Update 구간 합 구하기 4.java

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -5,27 +5,6 @@ import java.io.InputStreamReader;
 import java.awt.*;
 
 public class Main {
-    public static int solution(int n, int k, int[] arr) { // 최소 m미터 위한 최대 절단기 높이
-        int answer = -1;
-        TreeSet<Integer> Tset = new TreeSet<>(Collections.reverseOrder()); // sorted set
-        for (int i=0; i<n; i++) {
-            for (int j=i+1; j<n; j++) {
-                for (int l=j+1; l<n; l++) {
-                    Tset.add(arr[i] + arr[j] + arr[l]);
-                }
-            }
-        }
-        
-        int cnt = 0;
-        for (int x : Tset) {
-            cnt++;
-            if (cnt == k) {
-                return x;
-            }
-        }
-        return answer;
-    } 
-    
     public static void main(String args[]) throws IOException {
       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
       StringTokenizer st = new StringTokenizer(br.readLine());
