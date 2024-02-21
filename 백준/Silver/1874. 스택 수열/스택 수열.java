922a61f4aea22c0358be4a3859499563336030b4
Gayeong Park <maroony55@gmail.com>
2024-02-21 13:59:54 +0900

    [Silver II] Title: 스택 수열, Time: 1076 ms, Memory: 28148 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -2,45 +2,48 @@ import java.util.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.awt.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+
+    public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        StringBuilder sb = new StringBuilder();
         int[] arr = new int[n+1];
-        for (int i=1; i<=n; i++) {
+        for (int i=1; i<n+1; i++) {
             arr[i] = Integer.parseInt(br.readLine());
         }
         
         Stack<Integer> s = new Stack<>();
-        int count = 0; // 스택에 넣은 개수
+        int count = 0; // 어디까지 넣었는지 나타냄
+        StringBuilder sb = new StringBuilder();
         
-        for (int i=1; i<=n; i++) { // 꺼내야 하는 것
-            if (s.search(arr[i]) == -1) {
-                // 더 넣어야 하는 경우
-                if (count < arr[i]){
-                    for (int j=count+1; j<=arr[i]; j++) {
+        for (int i=1; i<=n; i++) {
+            if (s.search(arr[i]) == -1) {// 스택에서 꺼내야하는 수가 없으면
+                if (count < arr[i]) { // 더 넣으면 되는 경우
+                    for (int j=count+1; j<= arr[i]; j++) {
                         s.push(j);
                         sb.append('+').append('\n');
+                        count++;
                     }
                     s.pop();
                     sb.append('-').append('\n');
-                    count = arr[i];
-                } else {
+                } else { // 더 넣을 수 없는 경우
                     System.out.println("NO");
                     return;
                 }
             } else {
-                int tmp = s.search(arr[i]);
-                for (int j=0; j<tmp; j++) {
-                    s.pop();
+                while (s.pop() != arr[i]) {
                     sb.append('-').append('\n');
                 }
+                sb.append('-').append('\n');
             }
+              
         }
         
         System.out.println(sb);
     }
+    
+   
     
 }
