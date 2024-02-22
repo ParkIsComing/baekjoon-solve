1679f38028652e87b72a14fbb174d660317c45b6
Gayeong Park <maroony55@gmail.com>
2024-02-22 15:28:10 +0900

    [Silver IV] Title: 듣보잡, Time: 384 ms, Memory: 29284 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.java
+++ 백준/Silver/1764. 듣보잡/듣보잡.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
+    
     public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -21,21 +22,24 @@ public class Main {
             map.put(input, map.getOrDefault(input, 0) + 1);
         }
         
-        // Iterator를 사용하여 컬렉션을 순회하면서 삭제
+        //Iterator를 사용하여 맵을 순회하면서 듣보잡이 아니면 삭제
         Iterator<Map.Entry<String, Integer>> iterator = map.entrySet().iterator();
         while (iterator.hasNext()) {
             Map.Entry<String, Integer> entry = iterator.next();
             if (entry.getValue() == 1) {
-                iterator.remove(); // 반복자를 통해 안전하게 삭제
+                iterator.remove();
             }
         }
-        List<String> keySet = new ArrayList<>(map.keySet());
-        System.out.println(keySet.size());
         
-        // 키 값으로 오름차순 정렬
-        Collections.sort(keySet);
-        for(String key : keySet) {
-            System.out.println(key);
+        List<String> result = new ArrayList<>(map.keySet());
+        System.out.println(result.size());
+        
+        StringBuilder sb  = new StringBuilder();
+        Collections.sort(result);
+        for (String name : result) {
+            sb.append(name).append("\n");
         }
+        System.out.println(sb);
+
     }
 }
