89c29c5cb9de40430a368806732f3aecdc0c9212
Gayeong Park <maroony55@gmail.com>
2024-02-29 14:33:58 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 128 ms, Memory: 15924 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
@@ -3,67 +3,64 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-// 연산자를 하나씩 써보면서 재귀..
-
 public class Main {
-    public static int n,m;
-    public static int max = Integer.MIN_VALUE, min = Integer.MAX_VALUE;
-    public static int[] numbers = new int[100];
-    public static int[] operators = new int[4]; // + - * /
+    static int n;
+    static int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
+    static int[] num;
+    static int[] operator = new int[4];
+    static int o = 0;
+
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        num = new int[n];
+        StringTokenizer st= new StringTokenizer(br.readLine());
+        for (int i=0; i<n; i++) {
+            num[i] = Integer.parseInt(st.nextToken());
+        }
+        st = new StringTokenizer(br.readLine());
+        for (int i=0; i<4; i++) {
+            operator[i] = Integer.parseInt(st.nextToken());
+            o += operator[i];
+        }
+        
+        dfs(0, num[0]);
+        System.out.println(max);
+        System.out.println(min);
+        
+    }
     
-    public static void dfs(int sum, int count) { 
-        if (count == n) {
-            min = Math.min(sum, min);
+    // 벽 세우기
+    public static void dfs(int count, int sum) {
+        if (count == o) {
             max = Math.max(sum, max);
+            min = Math.min(sum, min);
             return;
         }
         
         for (int i=0; i<4; i++) {
-            if (operators[i] > 0) {
-                operators[i]--;
+            if (operator[i] > 0) {
+                operator[i]--; // 연산자 사용
                 
                 switch(i) {
                     case 0:
-                        dfs(sum + numbers[count], count+1);
+                        dfs(count+1, sum + num[count+1]);
                         break;
                     case 1:
-                        dfs(sum - numbers[count], count+1);
+                        dfs(count+1, sum - num[count+1]);
                         break;
                     case 2:
-                        dfs(sum * numbers[count], count+1);
+                        dfs(count+1, sum * num[count+1]);
                         break;
                     case 3:
-                        dfs(sum / numbers[count], count+1);
+                        dfs(count+1, sum /num[count+1]);
                         break;
                 }
-                
-                operators[i]++;
+                operator[i]++; // 백트래킹
             }
-            
-            
-            
         }
+        
+        
     }
     
-    public static void main(String args[]) throws IOException {
-      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-      n = Integer.parseInt(br.readLine());
-      
-      StringTokenizer st = new StringTokenizer(br.readLine());
-      for (int i=0; i<n; i++) {
-          numbers[i] = Integer.parseInt(st.nextToken());
-      }
-      
-      st = new StringTokenizer(br.readLine());
-      for (int i=0; i<4; i++) {
-          operators[i] = Integer.parseInt(st.nextToken());
-      }
-      
-      m = Arrays.stream(operators).sum();
-
-      dfs(numbers[0], 1);
-      
-      System.out.println(max);
-      System.out.println(min);
-    }
 }
