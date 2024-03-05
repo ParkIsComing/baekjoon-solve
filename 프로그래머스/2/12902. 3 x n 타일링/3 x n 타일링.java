4c18084ba33a355873c9e337e190aea2afc773d4
Gayeong Park <maroony55@gmail.com>
2024-03-05 11:05:34 +0900

    [level 2] Title: 3 x n 타일링, Time: 18.39 ms, Memory: 52.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12902. 3 x n 타일링/3 x n 타일링.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n) {
+        long[] dp = new long[n+1];
+        dp[0] = 1;
+        dp[1] = 0;
+        dp[2] = 3;
+
+        for (int i=4; i<=n; i++) {
+            //2개씩 잡으면 3가지 가능, 4개씩 잡으면 2가지 가능
+            dp[i] = (dp[i-2]*3)%1000000007;
+            
+            for (int j=i-4; j>=0; j=j-2) {
+                dp[i] += (dp[j]*2)%1000000007;
+            }
+        }
+        System.out.println(Arrays.toString(dp));
+        return (int)(dp[n]%1000000007);
+    }
+}
