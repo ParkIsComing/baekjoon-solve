23b7ee383a41d89ba850966bf4ffe2ae9933dbba
Gayeong Park <maroony55@gmail.com>
2024-06-18 16:45:36 +0900

    [Silver I] Title: 카잉 달력, Time: 192 ms, Memory: 15884 KB -BaekjoonHub

---
+++ 백준/Silver/6064. 카잉 달력/카잉 달력.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.io.*;
+
+
+public class Main {
+    static int n;
+    static int M,N,x,y;
+    
+    static int findLastYear() {
+        boolean check = false;
+        for (int i=x; i<N*M; i+=M) {
+            if (i % N == y) {
+                return i+1;
+            }
+        }
+        
+        return -1;
+    }
+    
+    public static void main(String args[]) throws IOException {
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      n = Integer.parseInt(br.readLine());
+      StringBuilder sb = new StringBuilder();
+      StringTokenizer st;
+      
+      for (int i=0; i<n; i++) {
+          st = new StringTokenizer(br.readLine());
+          M = Integer.parseInt(st.nextToken());
+          N = Integer.parseInt(st.nextToken());
+          x = Integer.parseInt(st.nextToken()) -1;
+          y = Integer.parseInt(st.nextToken()) -1;
+          
+          sb.append(findLastYear());
+          sb.append("\n");
+      }
+      
+      System.out.println(sb);
+      
+    }
+}
