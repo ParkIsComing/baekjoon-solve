4c6c8dd24615c1a1cb03d2bef5ef7e2eb5e4ce24
Gayeong Park <maroony55@gmail.com>
2024-09-27 10:42:14 +0900

    [level 2] Title: N-Queen, Time: 81.55 ms, Memory: 93.5 MB -BaekjoonHub

--- 프로그래머스/2/12952. N－Queen/N－Queen.java
+++ 프로그래머스/2/12952. N－Queen/N－Queen.java
@@ -2,19 +2,19 @@ import java.util.*;
 import java.awt.*;
 
 class Solution {
+    final int SIZE = 12;
+    boolean[] is_queen_in_col = new boolean[SIZE]; // 열
+    boolean[] is_queen_in_left = new boolean[SIZE * 2]; // 좌하향 대각선 (위의 행부터 두기 때문에 위쪽 대각선은 무시)
+    boolean[] is_queen_in_right = new boolean[SIZE * 2]; // 우하향 대각선
     int answer = 0;
     
     public int solution(int n) {
-        final int SIZE = 12;
-        boolean[] is_queen_in_col = new boolean[SIZE]; // 열
-        boolean[] is_queen_in_left = new boolean[SIZE * 2]; // 좌하향 대각선 (위의 행부터 두기 때문에 위쪽 대각선은 무시)
-        boolean[] is_queen_in_right = new boolean[SIZE * 2]; // 우하향 대각선
-        backtrack(0, n, is_queen_in_col,is_queen_in_left,is_queen_in_right);
+        backtrack(0, n);
         return answer;
     }
     
     // 행마다 하나씩 놓아 보기
-    public void backtrack(int row, int n, boolean[] is_queen_in_col, boolean[] is_queen_in_left, boolean[] is_queen_in_right) {
+    public void backtrack(int row, int n) {
         if (row == n) {
             answer++;
             return;
@@ -26,7 +26,7 @@ class Solution {
                 is_queen_in_left[row+col] = true;
                 is_queen_in_right[row-col+n] = true;
                 
-                backtrack(row+1, n,is_queen_in_col, is_queen_in_left, is_queen_in_right);
+                backtrack(row+1, n);
                 is_queen_in_col[col] = false;
                 is_queen_in_left[row+col] = false;
                 is_queen_in_right[row-col+n] = false;
