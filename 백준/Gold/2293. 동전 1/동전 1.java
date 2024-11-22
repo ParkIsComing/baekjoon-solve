d4daaaaeda405332a12592c2d90d28eeb877f26d
Gayeong Park <maroony55@gmail.com>
2024-11-23 05:51:44 +0900

    [Gold IV] Title: 동전 1, Time: 208 ms, Memory: 17624 KB -BaekjoonHub

---
+++ 백준/Gold/2293. 동전 1/동전 1.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        int k = scanner.nextInt();
+
+        int[] coins = new int[n];
+        for (int i = 0; i < n; i++) {
+            coins[i] = scanner.nextInt();
+        }
+
+        int[] dp = new int[k + 1];
+        dp[0] = 1;
+
+        for (int coin : coins) {
+            for (int i = coin; i <= k; i++) {
+                dp[i] += dp[i - coin];
+            }
+        }
+
+        System.out.println(dp[k]);
+    }
+}
