7a2fa88d4114f85c952de36ecc22339f6f0da792
Gayeong Park <maroony55@gmail.com>
2025-09-13 21:23:20 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 180 ms, Memory: 17708 KB -BaekjoonHub

---
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+ 
+public class Main {
+    // 재귀
+	public static int gcd(int a, int b) {
+		if (b == 0)
+			return a;
+            
+		return gcd(b, a % b);
+	}
+	public static void main(String[] args) {
+ 
+		Scanner in = new Scanner(System.in);
+		
+		int a = in.nextInt();
+		int b = in.nextInt();
+		int d = gcd(a, b);	// 최대공약수
+ 
+		System.out.println(d);
+		System.out.println(a * b / d);
+ 
+	}
+}
