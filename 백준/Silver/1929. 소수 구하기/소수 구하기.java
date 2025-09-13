60d4d8be7b16d855c007f7fcb382327094ee43fa
Gayeong Park <maroony55@gmail.com>
2025-09-13 21:40:46 +0900

    [Silver III] Title: 소수 구하기, Time: 256 ms, Memory: 20884 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -1,38 +1,33 @@
+import java.io.*;
 import java.util.*;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String args[]) throws IOException {
-      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-      StringTokenizer st = new StringTokenizer(br.readLine());
-      int m = Integer.parseInt(st.nextToken());
-      int n = Integer.parseInt(st.nextToken());
-      boolean[] isPrime = new boolean[n+1];
-      Arrays.fill(isPrime, true);
-      
-      isPrime[0] = false;
-      isPrime[1] = false;
-      
-      StringBuilder sb = new StringBuilder();
-      for (int i=2; i*i <= n; i++) {
-          if (isPrime[i]) {
-              for (int j = i*i; j<=n; j=j+i) {
-                  isPrime[j] = false;
-              }          
-              
-          }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-      }
-      
-      for (int i=m; i<=n; i++) {
-          if (isPrime[i]) {
-              sb.append(i).append("\n");
-          }
-      }
-      
-      System.out.println(sb);
-      
+        int start = Integer.parseInt(st.nextToken());
+        int end = Integer.parseInt(st.nextToken());
+
+        boolean[] isPrime = new boolean[end + 1];
+        Arrays.fill(isPrime, true);
+        isPrime[0] = isPrime[1] = false;
+
+        for (int i = 2; i * i <= end; i++) {
+            if (isPrime[i]) {
+                for (int j = i * i; j <= end; j += i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = start; i <= end; i++) {
+            if (isPrime[i]) {
+                sb.append(i).append("\n");
+            }
+        }
+
+        System.out.print(sb);
     }
 }
