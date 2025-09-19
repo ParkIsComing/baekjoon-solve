c1153dc8f2a13ce26114a43f9355b6835c46e242
Gayeong Park <maroony55@gmail.com>
2025-09-19 16:15:21 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 104 ms, Memory: 14248 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -1,28 +1,27 @@
 import java.util.*;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.awt.*;
+import java.io.*;
 
 public class Main {
-    public static int solution(int num) { // 최소 m미터 위한 최대 절단기 높이
-        int[] dp = new int[11];
-        dp[1] = 1;
-        dp[2] = 2;
-        dp[3] = 4;
-        for (int i=4; i<=num; i++) {
-            dp[i] = dp[i-1] + dp[i-2]+ dp[i-3];
-        }
-        
-        return dp[num];
-    } 
-    
-    public static void main(String args[]) throws IOException {
-      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-      int n = Integer.parseInt(br.readLine());
-      for (int i=0; i<n; i++) {
-          int answer = solution(Integer.parseInt(br.readLine()));
-          System.out.println(answer);
-      }
-    }
+	public static final int MAX_N = 11;
+	public static int solution(int num) { // 최소 m미터 위한 최대 절단기 높이
+		int[] dp = new int[MAX_N];
+		dp[1] = 1;
+		dp[2] = 2;
+		dp[3] = 4;
+		for (int i=4; i<=num; i++) {
+			dp[i] = dp[i-1] + dp[i-2]+ dp[i-3];
+		}
+
+		return dp[num];
+	}
+
+	public static void main(String args[]) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		for (int i=0; i<n; i++) {
+			int input = Integer.parseInt(br.readLine());
+			int answer = solution(input);
+			System.out.println(answer);
+		}
+	}
 }
