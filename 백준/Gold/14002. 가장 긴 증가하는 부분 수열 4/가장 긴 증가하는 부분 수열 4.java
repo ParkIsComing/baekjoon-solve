6b54e48510c77c86cdb3308bf467c624109d3b0e
Gayeong Park <maroony55@gmail.com>
2025-10-02 16:05:58 +0900

    [Gold IV] Title: 가장 긴 증가하는 부분 수열 4, Time: 112 ms, Memory: 14332 KB -BaekjoonHub

--- 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.java
+++ 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.java
@@ -1,63 +1,67 @@
+import java.io.*;
 import java.util.*;
 
 public class Main {
     static int n;
-    static ArrayList<Integer> seq = new ArrayList<>();
-    static ArrayList<Integer> lis = new ArrayList<>(); // tails
-    static int[] level; // level[i] = A[i]가 lis에서 차지한 위치(0-based)
+    static int[] seq;
+    static int[] lis;    
+    static int[] level;  // for backtrace
 
-    static int findLowerBound(int start, int end, int target) {
-        while (start < end) {
-            int mid = (start + end) / 2;
-            if (lis.get(mid) >= target) end = mid;
-            else start = mid + 1;
+    static int findLowerBound(int end, int target) {
+        int l = 1, r = end;
+        while (l < r) {
+            int mid = (l + r) >>> 1;
+            if (lis[mid] >= target) r = mid;
+            else l = mid + 1;
         }
-        return start;
+        return l;
     }
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        for (int i = 0; i < n; i++) seq.add(sc.nextInt());
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine().trim());
 
-        level = new int[n];
+        seq = new int[n+1];
+        level = new int[n+1];
+        lis = new int[n+1];
 
-        lis.add(seq.get(0));
-        level[0] = 0;
-        int lisIdx = 0;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= n; i++) {
+            seq[i] = Integer.parseInt(st.nextToken());
+        }
 
-        for (int i = 1; i < n; i++) {
-            int x = seq.get(i);
-            if (x > lis.get(lisIdx)) {
-                lis.add(x);
+        int lisIdx = 1;
+        lis[lisIdx] = seq[1];
+        level[1] = lisIdx;
+
+        for (int i = 2; i <= n; i++) {
+            int cur = seq[i];
+            if (cur > lis[lisIdx]) {
                 lisIdx++;
+                lis[lisIdx] = cur;
                 level[i] = lisIdx;
             } else {
-                int idx = findLowerBound(0, lisIdx + 1, x);
-                lis.set(idx, x);
+                int idx = findLowerBound(lisIdx + 1, cur);
+                lis[idx] = cur;
                 level[i] = idx;
             }
         }
 
-        int length = lisIdx + 1;
+        int length = lisIdx;
         System.out.println(length);
 
-        // 역추적
-        int[] ans = new int[length];
-        int need = length - 1;
-        int last = Integer.MAX_VALUE;
-        for (int i = n - 1; i >= 0 && need >= 0; i--) {
-            int val = seq.get(i);
-            if (level[i] == need && val < last) {
-                ans[need] = val;
-                last = val;
+        int[] ans = new int[length+1];
+        int need = length;
+        for (int i = n; i >= 1 && need >= 1; i--) {
+            if (level[i] == need) {
+                ans[need] = seq[i];
                 need--;
             }
         }
 
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < length; i++) {
-            if (i > 0) sb.append(' ');
+        for (int i = 1; i <= length; i++) {
+            if (i > 1) sb.append(' ');
             sb.append(ans[i]);
         }
         System.out.println(sb.toString());
