d37c9a2796fd04541f30ab12df96fb96356ec132
Gayeong Park <maroony55@gmail.com>
2025-10-02 16:22:12 +0900

    [Silver II] Title: 상자넣기, Time: 112 ms, Memory: 14360 KB -BaekjoonHub

---
+++ 백준/Silver/1965. 상자넣기/상자넣기.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int n;
+    static int[] seq;
+    static int[] lis;    
+
+    static int findLowerBound(int end, int target) {
+        int l = 1, r = end;
+        while (l < r) {
+            int mid = (l + r) >>> 1;
+            if (lis[mid] >= target) r = mid;
+            else l = mid + 1;
+        }
+        return l;
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine().trim());
+
+        seq = new int[n+1];
+        lis = new int[n+1];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= n; i++) {
+            seq[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int lisIdx = 1;
+        lis[lisIdx] = seq[1];
+
+        for (int i = 2; i <= n; i++) {
+            int cur = seq[i];
+            if (cur > lis[lisIdx]) {
+                lisIdx++;
+                lis[lisIdx] = cur;
+            } else {
+                int idx = findLowerBound(lisIdx + 1, cur);
+                lis[idx] = cur;
+            }
+        }
+
+        System.out.println(lisIdx);
+
+    }
+}
