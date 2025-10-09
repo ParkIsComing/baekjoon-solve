ed73df513def694c0baccab01979a4c5332935ad
Gayeong Park <maroony55@gmail.com>
2025-10-09 18:10:19 +0900

    [Gold V] Title: 경쟁적 전염, Time: 264 ms, Memory: 21892 KB -BaekjoonHub

--- 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.java
+++ 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.java
@@ -1,92 +1,84 @@
+import java.io.*;
 import java.util.*;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
-class Node {
-    int type;
-    int sec;
-    int x;
-    int y;
-    
-    Node (int type, int sec, int x, int y) {
-        this.type = type;
-        this.sec = sec;
-        this.x = x;
-        this.y = y;
-    }
-}
 
 public class Main {
-    static int n, e, s, x, y;
-    static int[][] map;
-    static ArrayList<Node> list = new ArrayList<>();
-    static Queue<Node> q = new LinkedList<>();
-    static int[][] direction = {{-1,0}, {1, 0}, {0,1}, {0,-1}};
-    
-    public static void bfs() {
-        while (!q.isEmpty()) {
-            Node cur = q.poll();
-            
-            if (cur.sec == s) {
-                break;
-            }
-            
-            for (int[] d : direction) {
-                int nx = cur.x + d[0];
-                int ny = cur.y + d[1]; 
-                
-                if (0 > nx || nx > n || 0 > ny || ny > n) {
-                    continue;
-                }
-                
-                if (map[nx][ny] == 0) {
-                    map[nx][ny] = cur.type;
-                    q.offer(new Node(cur.type, cur.sec+1, nx, ny));
-                }
-            }
-        }
-        
-    }
+	static final int MAX_N = 200;
+	static final int[][] DIR = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+	static int N,K,S,X,Y;
+	static int[][] map = new int[MAX_N+1][MAX_N+1]; // 1-based
+	static PriorityQueue<Cell> pq = new PriorityQueue<>();
 
-    public static void main(String args[]) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        
-        n = Integer.parseInt(st.nextToken()); // n*n map
-        e = Integer.parseInt(st.nextToken()); // 바이러스 종류
-        
-        map = new int[n+1][n+1];
-    
-        for (int i=1; i<=n; i++) {
-            st = new StringTokenizer(br.readLine()); 
-            for (int j=1; j<=n; j++) {
-                int input = Integer.parseInt(st.nextToken());
-                map[i][j] = input;
-                if (input != 0) { // 바이러스 정보 저장
-                    list.add(new Node(input, 0, i, j));
-                }
-            }
-        }
-        st = new StringTokenizer(br.readLine());
-        s = Integer.parseInt(st.nextToken());
-        x = Integer.parseInt(st.nextToken());
-        y = Integer.parseInt(st.nextToken());
-        
-        Collections.sort(list, new Comparator<Node>() {
-            @Override
-            public int compare(Node n1, Node n2) {
-                return n1.type - n2.type; // 번호가 작은 바이러스부터 확산
-            }
-        });
-        
-        // 큐에 다 넣고 시작
-        for (Node n : list) {
-            q.offer(n);
-        }
-        
-        bfs();
+	public static void main(String[] args) throws IOException {
+		input();
+		spreadVirus();
+		System.out.println(map[X][Y]);
+	}
 
-        System.out.println(map[x][y]);
-    }
+	private static void input() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+		for (int i = 1; i <= N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 1; j <= N; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+				if (map[i][j] != 0) {
+					pq.offer(new Cell(i,j,map[i][j],0));
+				}
+			}
+		}
+		st = new StringTokenizer(br.readLine());
+		S = Integer.parseInt(st.nextToken());
+		X = Integer.parseInt(st.nextToken());
+		Y = Integer.parseInt(st.nextToken());
+	}
+
+	private static void spreadVirus() {
+		while (!pq.isEmpty()) {
+			Cell cur = pq.poll();
+			if (cur.sec == S) { // S초간 확산이 완료된 경우
+				break;
+			}
+			for (int[] dir : DIR) {
+				int nx = cur.x + dir[0];
+				int ny = cur.y + dir[1];
+				if (isInRange(nx, ny) && map[nx][ny] == 0) {
+					map[nx][ny] = cur.status;
+					pq.offer(new Cell(nx, ny, map[nx][ny], cur.sec + 1	));
+				}
+			}
+		}
+	}
+
+	private static boolean isInRange(int x, int y) {
+		return x>=1 && x<=N && y>=1 && y<=N;
+	}
 }
+
+class Cell implements Comparable<Cell> {
+	int x;
+	int y;
+	int status;
+	int sec;
+
+	public Cell(int x, int y, int status, int sec) {
+		this.x = x;
+		this.y = y;
+		this.status = status;
+		this.sec = sec;
+	}
+
+	@Override
+	public int compareTo(Cell cell) {
+		if (this.sec != cell.sec) {
+			return this.sec - cell.sec;
+		} else {
+			return this.status - cell.status;
+		}
+	}
+}
+
+// 작은 숫자부터 확산
+// 바이러스가 없는 곳에는 확산 가능(상하좌우), 덮어쓰기 불가능
+// 바이러스 없으면 0, 있으면 자연수
