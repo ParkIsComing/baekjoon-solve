a13a77ecb32329c0d6270fec6a127cf394c69759
Gayeong Park <maroony55@gmail.com>
2025-10-30 15:40:06 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 648 ms, Memory: 106856 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
@@ -1,76 +1,98 @@
-import java.util.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.awt.Point;
+import java.util.*;
+
+
+// 0 이동가능 / 1 벽
+// 벽 하나 부술 수 있음
+// 최단경로로 이동
+
+class Node {
+    int x;
+    int y;
+    int depth;
+    int isBroken;
+
+    Node(int x, int y, int depth, int isBroken) {
+        this.x = x;
+        this.y = y;
+        this.depth = depth;
+        this.isBroken = isBroken;
+    }
+}
 
 public class Main {
-    static int n, m;
+    static final int[][] DIRECTION = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+    static int n,m;
     static int[][] map;
-    static int[][][] visited;
-    static int[][] direction = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
+    static boolean[][][] visited;
+    static Deque<Node> q = new ArrayDeque<>();
 
-    public static void main(String args[]) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        map = new int[n + 1][m + 1]; 
-        visited = new int[n + 1][m + 1][2]; 
-
-        for (int i = 1; i <= n; i++) {
-            String input = br.readLine();
-            for (int j = 1; j <= m; j++) {
-                map[i][j] = input.charAt(j - 1) - '0';
-            }
-        }
-
-        int result = bfs();
-        System.out.println(result);
+    public static void main(String[] args) throws IOException {
+        input();
+        System.out.println(bfs());
     }
 
-    private static int bfs() {
-        Queue<PointState> queue = new LinkedList<>();
-        queue.offer(new PointState(1, 1, 0));
-        visited[1][1][0] = 1; 
+    static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
 
-        while (!queue.isEmpty()) {
-            PointState current = queue.poll();
+        map = new int[n][m];
+        visited = new boolean[n][m][2]; // 0: 벽을 안 부숨/ 1: 벽을 부숨
+        for (int i=0; i<n; i++) {
+            String input = br.readLine();
+            for (int j=0; j<m; j++) {
+                map[i][j] = input.charAt(j) - '0';
+            }
+        }
+    }
+    
+    static int bfs() {
 
-            if (current.x == n && current.y == m) {
-                return visited[current.x][current.y][current.breakWall];
+        q.offer(new Node(0,0,1, 0));
+        visited[0][0][0] = true;
+
+        while (!q.isEmpty()) {
+            Node cur =  q.poll();
+            if(cur.x == n-1 && cur.y == m-1) {
+                return cur.depth;
             }
 
-            for (int[] d : direction) {
-                int dx = current.x + d[0];
-                int dy = current.y + d[1];
+            for (int[] d : DIRECTION) {
+                int nx = cur.x+  d[0];
+                int ny = cur.y +  d[1];
 
-                if (dx < 1 || dx > n || dy < 1 || dy > m) {
-                    continue;
-                }
+                if (inRange(nx, ny)) {
+                    // 0이면 그냥 이동
+                    if (map[nx][ny] == 0) {
+                        if (!visited[nx][ny][cur.isBroken]) {
+                            visited[nx][ny][cur.isBroken] = true;
+                            q.offer(new Node(nx,ny,cur.depth+1, cur.isBroken));
+                        }
+                    } else {
+                        // 1인 경우
+                        // 아직 벽을 안 부쉈으면 부수고 이동
+                        if (cur.isBroken == 0) {
+                            if (!visited[nx][ny][1]) {
+                                visited[nx][ny][1] = true;
+                                q.offer(new Node(nx,ny, cur.depth+1, 1));
+                            }
+                        }
 
-                if (map[dx][dy] == 0 && visited[dx][dy][current.breakWall] == 0) {
-                    visited[dx][dy][current.breakWall] = visited[current.x][current.y][current.breakWall] + 1;
-                    queue.offer(new PointState(dx, dy, current.breakWall));
-                }
-
-                if (map[dx][dy] == 1 && current.breakWall == 0 && visited[dx][dy][1] == 0) {
-                    visited[dx][dy][1] = visited[current.x][current.y][0] + 1;
-                    queue.offer(new PointState(dx, dy, 1));
+                    }
                 }
             }
         }
+
         return -1;
     }
 
-    static class PointState {
-        int x, y, breakWall;
-
-        PointState(int x, int y, int breakWall) {
-            this.x = x;
-            this.y = y;
-            this.breakWall = breakWall;
-        }
+    static boolean inRange(int x, int y) {
+        return x >= 0 && x < n && y >= 0 && y < m;
     }
+
 }
