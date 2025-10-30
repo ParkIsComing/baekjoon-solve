1c549b2b901d32a7a2555ebe7f142570f02a63e5
Gayeong Park <maroony55@gmail.com>
2025-10-30 14:48:30 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 544 ms, Memory: 141620 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -1,60 +1,69 @@
-import java.util.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.*;
 
 public class Main {
-    public static int n,m;
-    public static ArrayList<ArrayList<Integer>> list = new ArrayList<ArrayList<Integer>>();
-    public static boolean[] visited = new boolean[1001];
-    
-    public static boolean bfs(int node) {
-        if (visited[node] == false) { // 미방문 노드
-            Queue<Integer> q = new LinkedList<>();
-            q.offer(node);
-            visited[node] = true;
-            
-            while (!q.isEmpty()) {
-                int x = q.poll();
-                for (int y : list.get(x)) {
-                    if (visited[y] == false) {
-                        q.offer(y);
-                        visited[y] = true;
-                    }
-                }
-                
-            }
-            return true;        
-            
+    static int n,m;
+    static ArrayList<Integer>[] graph;
+    static boolean[] visited;
+
+    public static void main(String[] args) throws IOException {
+        input();
+        System.out.println(findConnectComponent());
+
+    }
+
+    static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        graph = new ArrayList[n+1];
+        visited = new boolean[n+1];
+        for (int i = 0; i <= n; i++) {
+            graph[i] = new ArrayList<>();
         }
-        return false;
-    }
-    
-    public static void main(String args[]) throws IOException {
-      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-      StringTokenizer st = new StringTokenizer(br.readLine());
-      n = Integer.parseInt(st.nextToken());
-      m = Integer.parseInt(st.nextToken());
-      
-      for (int i=0; i<=n; i++) {
-          list.add(new ArrayList<Integer>());
-      }
-      
 
-      for (int i=0; i<m; i++) {
-          st = new StringTokenizer(br.readLine());
-          int a = Integer.parseInt(st.nextToken());
-          int b = Integer.parseInt(st.nextToken());
-          list.get(a).add(b); // 방향없는 그래프
-          list.get(b).add(a);
-      }
-      int count = 0;
-      for (int i=1; i<=n; i++) {
-          if (bfs(i)) {
-              count++;
-          }
-      }
-
-      System.out.println(count);
+        for (int i=0; i<m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            graph[a].add(b); // 무방향 그래프
+            graph[b].add(a);
+        }
     }
+
+    static int findConnectComponent() {
+        int count = 0;
+
+        for (int i=1; i<=n; i++) {
+            if (!visited[i]) {
+                count++;
+                bfs(i);
+            }
+        }
+
+        return count;
+    }
+
+    static void bfs(int start) {
+        Deque<Integer> q = new ArrayDeque<>();
+        q.offer(start);
+        visited[start] = true;
+
+        while (!q.isEmpty()) {
+            Integer cur = q.poll();
+            for (int i: graph[cur]) {
+                if (!visited[i]) {
+                    visited[i] = true;
+                    q.offer(i);
+                }
+            }
+        }
+    }
+
+
+
 }
