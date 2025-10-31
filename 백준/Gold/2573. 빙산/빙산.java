b29038a8da82a5c8bbbef9930ebcf686d95d7ba3
Gayeong Park <maroony55@gmail.com>
2025-10-31 20:33:49 +0900

    [Gold IV] Title: 빙산, Time: 584 ms, Memory: 164032 KB -BaekjoonHub

--- 백준/Gold/2573. 빙산/빙산.java
+++ 백준/Gold/2573. 빙산/빙산.java
@@ -1,109 +1,137 @@
+import java.awt.*;
+import java.io.*;
 import java.util.*;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.awt.Point;
+
 
 public class Main {
-    static int n, m;
-    static int[][] map;
-    static ArrayList<Point> glacier = new ArrayList<>();
-    static int[][] direction = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
-    static int[][] visited;
+	static final int[][] DIR = {{-1,0},{1,0},{0,1},{0,-1}};
+	static int n,m;
+	static int[][] map;
+	static boolean[][] visited;
+	static int[][] meltingLevel;
 
-    static void updateMelting() {
-        int[][] tempMap = new int[n][m];
-        ArrayList<Point> newGlacier = new ArrayList<>();
-        
-        for (Point p : glacier) {
-            int meltCount = 0;
-            for (int[] d : direction) {
-                int dx = d[0] + p.x;
-                int dy = d[1] + p.y;
-                if (0 <= dx && dx < n && 0 <= dy && dy < m && map[dx][dy] == 0) {
-                    meltCount++;
-                }
-            }
-            tempMap[p.x][p.y] = Math.max(map[p.x][p.y] - meltCount, 0);
-            if (tempMap[p.x][p.y] > 0) {
-                newGlacier.add(p); // 아직 남아있는 빙산을 기록
-            }
-        }
-        glacier = newGlacier;
-        map = tempMap; // 업데이트된 맵으로 변경
-    }
 
-    static int countGlacier(int time) {
-        int count = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (visited[i][j] != time && map[i][j] != 0) {
-                    bfs(i, j, time);
-                    count++;
-                }
-            }
-        }
-        return count;
-    }
+	public static void main(String[] args) throws IOException {
+		input();
+		System.out.println(solution());
+	}
 
-    static void bfs(int x, int y, int time) {
-        Queue<Point> q = new LinkedList<>();
-        q.offer(new Point(x, y));
-        visited[x][y] = time;
+	private static void input() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		map = new int[n][m];
 
-        while (!q.isEmpty()) {
-            Point p = q.poll();
-            for (int[] d : direction) {
-                int dx = d[0] + p.x;
-                int dy = d[1] + p.y;
-                if (0 <= dx && dx < n && 0 <= dy && dy < m) {
-                    if (visited[dx][dy] != time && map[dx][dy] != 0) {
-                        visited[dx][dy] = time;
-                        q.offer(new Point(dx, dy));
-                    }
-                }
-            }
-        }
-    }
+		for (int i = 0; i < n; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < m; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+	}
 
-    public static void main(String args[]) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+	private static int solution() {
+		int years = 0;
+		while (!isAllMelted()) {
+			if (isSplited()) {
+				return years;
+			}
+			melt();
+			years++;
+		}
+		return 0;
+	}
 
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
+	private static boolean isAllMelted() {
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				if (map[i][j] != 0) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+	private static void melt() {
+		meltingLevel = new int[n][m];
 
-        map = new int[n][m];
+		// 빙산이면(1이상) 바닷물과 접한 면의 수만큼 녹음
+		// 그 시점에 녹을 양을 저장
+		for (int i=0; i<n; i++) {
+			for (int j=0; j<m; j++) {
+				if (map[i][j] >= 1) {
+					int cnt = countSeaCoasts(i,j);
+					meltingLevel[i][j] = cnt;
+				}
+			}
+		}
 
-        for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < m; j++) {
-                int tmp = Integer.parseInt(st.nextToken());
-                map[i][j] = tmp;
+		// 한번에 녹을 양 반영 (녹은 결과가 영향을 주는 걸 막음)
+		for (int i=0; i<n; i++) {
+			for (int j=0; j<m; j++) {
+				if (meltingLevel[i][j] > 0) {
+					map[i][j] = map[i][j] < meltingLevel[i][j] ? 0 : map[i][j] - meltingLevel[i][j];
+				}
+			}
+		}
+	}
 
-                if (tmp != 0) {
-                    glacier.add(new Point(i, j));
-                }
-            }
-        }
+	private static int countSeaCoasts(int x, int y) {
+		int cnt = 0;
+		for (int[] dir : DIR) {
+			int nx = x + dir[0];
+			int ny = y + dir[1];
 
-        int time = 0;
-        visited = new int[n][m];
-        while (true) {
-            time++;
-            updateMelting();
+			if (inRange(nx, ny) && map[nx][ny] == 0) {
+				cnt++;
+			}
+		}
+		return cnt;
+	}
 
-            if (glacier.isEmpty()) { // 모든 빙산이 녹아 없어졌다면
-                time = 0;
-                break;
-            }
 
-            int count = countGlacier(time);
-            if (count >= 2) {
-                break;
-            }
-        }
+	private static boolean inRange(int x, int y) {
+		return x >= 0 && x < n && y >= 0 && y < m;
+	}
 
-        System.out.println(time);
-    }
+	// 두 덩어리 이상으로 나뉘었는지 확인
+	private static boolean isSplited() {
+		visited = new boolean[n][m];
+		int cnt = 0;
+
+		for (int i=0; i<n; i++) {
+			for (int j=0; j<m; j++) {
+				if (!visited[i][j] && map[i][j] > 0) {
+					bfs(i, j);
+					cnt++;
+					if (cnt >= 2) {
+						return true;
+					}
+				}
+			}
+		}
+
+		return false;
+	}
+
+	private static void bfs(int x, int y) {
+		Deque<Point> queue = new ArrayDeque<>();
+		queue.offer(new Point(x, y));
+		visited[x][y] = true;
+
+		while (!queue.isEmpty()) {
+			Point p = queue.poll();
+
+			for (int[] dir : DIR) {
+				int nx = p.x + dir[0];
+				int ny = p.y + dir[1];
+				if (inRange(nx, ny) && map[nx][ny] > 0 && !visited[nx][ny]) {
+					visited[nx][ny] = true;
+					queue.offer(new Point(nx, ny));
+				}
+			}
+		}
+	}
+
 }
