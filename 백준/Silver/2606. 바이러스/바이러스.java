f525b968f4c94746aaaa0ed7085bccb69169ced4
Gayeong Park <maroony55@gmail.com>
2025-10-31 19:37:29 +0900

    [Silver III] Title: 바이러스, Time: 100 ms, Memory: 14096 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,54 +1,65 @@
-import java.util.*;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.awt.*;
+import java.io.*;
+import java.util.*;
+
+// 첨에 집 개수 정렬에서 출력하라는 거 놓쳐서 틀림
 
 public class Main {
-    static ArrayList<ArrayList<Integer>> graph = new ArrayList<ArrayList<Integer>>();
-    static int n;
-    static boolean[] d = new boolean[101];
-    
-    public static int bfs(int start) {
-        Queue<Integer> q = new LinkedList<>();
-        q.offer(start);
-        d[start] = true;
-        
-        int answer = 0;
-        while (!q.isEmpty()) {
-            int x = q.poll();
-            for (int i : graph.get(x)) {
-                if (d[i] == false) {
-                    d[i] = true; // 연결 노드 방문
-                    answer++;
-                    q.offer(i);
-                }
-            }
-                    
-        }
-        
-        return answer;
-    }
-    
-    public static void main(String args[]) throws IOException {
-      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-      int n = Integer.parseInt(br.readLine()); // 노드 수
-      int m = Integer.parseInt(br.readLine());// 간선 수
+	static int n,m;
+	static ArrayList<Integer>[] graph;
+	static boolean[] visited;
 
-      StringTokenizer st;
-      
-      for (int i=0; i<=n; i++) {
-          graph.add(new ArrayList<Integer>());
-      }
-      for (int i=0; i<m; i++) {
-          st = new StringTokenizer(br.readLine());
-          int s = Integer.parseInt(st.nextToken());
-          int e = Integer.parseInt(st.nextToken());
-          
-          graph.get(s).add(e);
-          graph.get(e).add(s);
-      }
-      
-      System.out.println(bfs(1));
-    }
+	public static void main(String[] args) throws IOException {
+		input();
+		System.out.println(bfs(1));
+	}
+
+	private static void input() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		n = Integer.parseInt(br.readLine());
+		m = Integer.parseInt(br.readLine());
+
+		graph = new ArrayList[n+1];
+
+		for (int i=0; i<=n; i++) {
+			graph[i] = new ArrayList<>();
+		}
+		visited = new boolean[n+1];
+
+		int a=0, b=0;
+		StringTokenizer st;
+		for (int i=0; i<m; i++) {
+			st = new StringTokenizer(br.readLine());
+			a = Integer.parseInt(st.nextToken());
+			b = Integer.parseInt(st.nextToken());
+			graph[a].add(b);
+			graph[b].add(a);
+		}
+	}
+
+
+	private static int bfs(int node) {
+		int count = 0;
+		Deque<Integer> q = new ArrayDeque<>();
+		q.offer(node);
+		visited[node] = true;
+
+		while (!q.isEmpty()) {
+			Integer cur = q.poll();
+			for (int i: graph[cur]) {
+				if (!visited[i]) {
+					visited[i] = true;
+					count++;
+					q.offer(i);
+				}
+			}
+		}
+
+		return count;
+	}
+
+	private static boolean inRange(int x, int y) {
+		return x >= 0 && x < n && y >= 0 && y < n;
+	}
+
 }
