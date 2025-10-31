3ffc6ff3347c57638c934b0a8ed86e79fffd9737
Gayeong Park <maroony55@gmail.com>
2025-10-31 19:10:30 +0900

    [Silver I] Title: 미로 탐색, Time: 112 ms, Memory: 14680 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,50 +1,56 @@
-import java.util.*;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.awt.*;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static int[][] graph;
-    public static int[][] directions = {{-1,0}, {1,0}, {0,-1}, {0,1}};
-    public static int n,m;
-    
-    public static int bfs(int x, int y) {
-        Queue<Point> q = new LinkedList<>();
-        q.offer(new Point(x,y));
-        
-        while (!q.isEmpty()) {
-            Point p = q.poll();
-            for (int[] d : directions) {
-                int nx = p.x + d[0];
-                int ny = p.y + d[1];
-                if (0<=nx && nx<n && 0<=ny && ny<m) {
-                    if (graph[nx][ny] == 1) {
-                        graph[nx][ny] = graph[p.x][p.y] + 1;
-                        q.offer(new Point(nx, ny));
-                    }                
-                }
+	static final int[][] DIR = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
+	static int n,m;
+	static int[][] maze;
 
-            }
-        }
-        
-        return graph[n-1][m-1];
-    }
-    
-    public static void main(String args[]) throws IOException {
-      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-      StringTokenizer st = new StringTokenizer(br.readLine());
-      n = Integer.parseInt(st.nextToken());
-      m = Integer.parseInt(st.nextToken());
-      graph = new int[n][m];
-      
-      for (int i =0; i<n; i++) {
-          String input = br.readLine();
-          for (int j=0; j<m; j++) {
-              graph[i][j] = (int)(input.charAt(j) -'0');
-          }
-      }
-      
-      System.out.println(bfs(0,0));
-    }
+
+	public static void main(String[] args) throws IOException {
+		input();
+		bfs(0,0);
+		System.out.println(maze[n-1][m-1]);
+	}
+
+	private static void input() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		maze = new int[n][m];
+
+		for (int i=0; i<n; i++) {
+			String s = br.readLine();
+			for (int j=0; j<m; j++) {
+				maze[i][j] = s.charAt(j) - '0';
+			}
+		}
+	}
+
+	private static void bfs(int x, int y) {
+		Deque<Point> q = new ArrayDeque<>();
+		q.offer(new Point(x, y));
+
+		while (!q.isEmpty()) {
+			Point p = q.poll();
+
+			for (int[] dir : DIR) {
+				int nx = p.x + dir[0];
+				int ny = p.y + dir[1];
+
+				if (inRange(nx, ny) && maze[nx][ny] == 1) {
+					maze[nx][ny] = maze[p.x][p.y] + 1;
+					q.offer(new Point(nx, ny));
+				}
+			}
+		}
+	}
+
+	private static boolean inRange(int x, int y) {
+		return x >= 0 && x < n && y >= 0 && y < m;
+	}
+
+
 }
