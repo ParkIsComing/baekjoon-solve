911c5293f3eb3ce4bdd69f38c397a740d516159a
Gayeong Park <maroony55@gmail.com>
2025-10-31 19:57:09 +0900

    [Silver II] Title: 유기농 배추, Time: 144 ms, Memory: 15928 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.java
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -1,61 +1,72 @@
+import java.awt.*;
+import java.io.*;
 import java.util.*;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 
 
 public class Main {
-    static int m, n;
-    static int[][] graph;
-    static boolean[][] visited;
-    static int[][] directions = {{-1,0}, {1,0}, {0,-1},{0,1}};
-    
-    public static void dfs(int x, int y) {
-        visited[x][y] = true; // 방문 처리
-        for (int[] d: directions) {
-            int nx = x + d[0];
-            int ny = y + d[1];
-            
-            if (0<= nx && nx < m && 0 <= ny && ny <n) {
-                if (graph[nx][ny] == 1 && !visited[nx][ny]) {
-                    dfs(nx, ny);
-                }
-            }
-        }
-    }
-    
-    public static void main(String args[]) throws IOException {
-      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-      int t = Integer.parseInt(br.readLine()); // 테스트케이스 수
-      StringTokenizer st;
-      
-      for (int i=0; i<t; i++) {
-        int count = 0;
-        st = new StringTokenizer(br.readLine());
-        m = Integer.parseInt(st.nextToken()); // 가로
-        n = Integer.parseInt(st.nextToken()); // 세로
-        int k = Integer.parseInt(st.nextToken()); // 배추 위치 개수
-        graph = new int[m][n];
-        visited = new boolean[m][n];
+	static final int[][] DIR = {{-1,0},{1,0},{0,1},{0,-1}};
+	static int t;
+	static int n,m,k;
+	static int[][] map;
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
 
-        for (int j=0; j<k; j++) {
-            st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
-            graph[x][y] = 1;
-        }
-        
-        for (int j =0; j<m; j++) {
-            for (int l=0; l<n; l++) {
-                if (graph[j][l] == 1 && !visited[j][l]) { // 배추가 있고 아직 방문하지 않은 장소 
-                    dfs(j,l);
-                    count++;
-                }
-            }
-        }
-        System.out.println(count);
-      }
-        
-    }
-    
+	public static void main(String[] args) throws IOException {
+		t = Integer.parseInt(br.readLine());
+		for (int i = 0; i < t; i++) {
+			solution();
+		}
+	}
+
+
+	private static void solution() throws IOException {
+		st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		k = Integer.parseInt(st.nextToken());
+
+		map = new int[n][m];
+		for (int i=0; i<k; i++) {
+			st = new StringTokenizer(br.readLine());
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+			map[x][y] = 1;
+		}
+
+		int count = 0;
+		for (int i=0; i<n; i++) {
+			for (int j=0; j<m; j++) {
+				if (map[i][j] == 1) {
+					bfs(i, j);
+					count++;
+				}
+			}
+		}
+
+		System.out.println(count);
+	}
+
+
+	private static void bfs(int x, int y) {
+		Deque<Point> q = new ArrayDeque<>();
+		q.offer(new Point(x, y));
+
+		while (!q.isEmpty()) {
+			Point p = q.poll();
+
+			for (int[] d : DIR) {
+				int nx = p.x + d[0];
+				int ny = p.y + d[1];
+				if (inRange(nx, ny) && map[nx][ny] == 1) {
+					map[nx][ny] = map[p.x][p.y] + 1;
+					q.offer(new Point(nx, ny));
+				}
+			}
+		}
+	}
+
+	private static boolean inRange(int x, int y) {
+		return x >= 0 && x < n && y >= 0 && y < m;
+	}
+
 }
