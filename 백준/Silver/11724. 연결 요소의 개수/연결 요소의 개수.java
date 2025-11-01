5eb888398f6bdfc0dde6ca968a7960e8dc04ee42
Gayeong Park <maroony55@gmail.com>
2025-11-01 22:57:56 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 444 ms, Memory: 114140 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -4,66 +4,53 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    static int n,m;
-    static ArrayList<Integer>[] graph;
-    static boolean[] visited;
+	static int n,m;
+	static ArrayList<Integer>[] graph;
+	static int[] parent;
 
-    public static void main(String[] args) throws IOException {
-        input();
-        System.out.println(findConnectComponent());
+	public static void main(String[] args) throws IOException {
+		input();
+		Set<Integer> set = new HashSet<>();
+		for (int i = 1; i <= n; i++) {
+			set.add(find(parent[i]));
+		}
+		
+		System.out.println(set.size());
+	}
 
-    }
+	static void input() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
 
-    static void input() throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
+		graph = new ArrayList[n+1];
+		parent = new int[n+1];
+		for (int i = 0; i <= n; i++) {
+			graph[i] = new ArrayList<>();
+			parent[i] = i;
+		}
 
-        graph = new ArrayList[n+1];
-        visited = new boolean[n+1];
-        for (int i = 0; i <= n; i++) {
-            graph[i] = new ArrayList<>();
-        }
+		for (int i=0; i<m; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			union(a, b);
+		}
+	}
 
-        for (int i=0; i<m; i++) {
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            graph[a].add(b); // 무방향 그래프
-            graph[b].add(a);
-        }
-    }
+	static void union(int x, int y) {
+		x = find(x);
+		y = find(y);
+		parent[y] = x;
+	}
 
-    static int findConnectComponent() {
-        int count = 0;
-
-        for (int i=1; i<=n; i++) {
-            if (!visited[i]) {
-                count++;
-                bfs(i);
-            }
-        }
-
-        return count;
-    }
-
-    static void bfs(int start) {
-        Deque<Integer> q = new ArrayDeque<>();
-        q.offer(start);
-        visited[start] = true;
-
-        while (!q.isEmpty()) {
-            Integer cur = q.poll();
-            for (int i: graph[cur]) {
-                if (!visited[i]) {
-                    visited[i] = true;
-                    q.offer(i);
-                }
-            }
-        }
-    }
-
-
+	static int find(int node) {
+		if (parent[node] == node) {
+			return node;
+		} else {
+			return parent[node] = find(parent[node]);
+		}
+	}
 
 }
