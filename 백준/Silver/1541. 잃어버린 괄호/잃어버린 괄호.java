46a76e97306156be35f8e9fc46756b480dac048b
Gayeong Park <maroony55@gmail.com>
2025-11-07 21:26:25 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 100 ms, Memory: 14396 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
@@ -1,38 +1,46 @@
-import java.util.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-
 public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String s = br.readLine().trim();
 
-    public static void main(String args[]) throws IOException {
-        
-      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-      String input = br.readLine();
-      int answer = 0;
-      
-      
-      // 최소가 되려면 빼기를 기준으로 우선 분리
-      String[] arr = input.split("-");
-      for(int i=0; i<arr.length; i++) {
-          String[] numbers = arr[i].split("\\+");
-          int tmp = 0;
-          for (String number: numbers) {
-              tmp += Integer.parseInt(number);
-          }
-          
-          if (i==0) {
-              answer += tmp;
-          } else {
-              answer -= tmp;
-          }
-      }
-      
-      System.out.println(answer);
-      
-      
-      
-    }
-    
+		int total = 0;       // 최종 결과
+		int curNum = 0;      // 현재 읽는 숫자
+		int groupSum = 0;    // '-'로 끊긴 구간(플러스들의 묶음) 합
+		boolean minusPhase = false; // 첫 '-' 이후인지
+
+		for (int i = 0; i < s.length(); i++) {
+			char c = s.charAt(i);
+
+			if (c >= '0' && c <= '9') {
+				curNum = curNum * 10 + (c - '0'); // 숫자 누적
+				continue;
+			}
+
+			groupSum += curNum;
+			curNum = 0;
+
+			if (c == '-') {
+				// 이전 그룹 정산
+				if (!minusPhase) total += groupSum;    // 첫 '-' 전: 더하기
+				else             total -= groupSum;    // 첫 '-' 후: 빼기
+				groupSum = 0;
+				minusPhase = true;
+			}
+			// '+'면 그룹만 유지 (아무 것도 안 함)
+		}
+
+		// 마지막 숫자/그룹 정산
+		groupSum += curNum;
+		if (!minusPhase) {
+			total += groupSum;
+		} else {
+			total -= groupSum;
+		}
+
+		System.out.println(total);
+	}
 }
